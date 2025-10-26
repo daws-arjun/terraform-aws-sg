1e81fe3298c80b5e276c44c17fb829387398a5f6
daws-arjun <a.nagarjuna97@gmail.com>
2025-10-26 17:15:35 +0530

    new

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
 resource "aws_security_group" "main" {
-  name        = var.sg_name
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  name        = var.sg_name # catalogue
+  description = var.sg_description
+  vpc_id      = var.vpc_id
 
-  tags = {
-    Name = "allow_tls"
+# we are not added ingress bcz we need to add diff ports
+# egress is common for all sg
+# egress traffic is instace traffic
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
+
+  tags = merge(
+    var.sg_tags,
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-${var.sg_name}" # roboshop-dev-catalogue
+    }
+  )
 }
